8797c3b78da71bc2a93d206a542423f7b04676c8
DayMoniakk <54359117+DayMoniakk@users.noreply.github.com>
2023-07-17 18:23:51 +0200

    Added new project

--- scripts/home.js
+++ scripts/home.js
@@ -7,10 +7,11 @@ for (let index = 0; index < projectButtons.length; index++) {
 
         switch (index){
             case 0: targetPage = "guncraft-duels.html"; break;
-            case 1: targetPage = "twitch-cat-overlay.html"; break;
-            case 2: targetPage = "go-chicken.html"; break;
-            case 3: targetPage = "strength-quest.html"; break;
-            case 4: targetPage = "operation-earthquake.html"; break;
+            case 1: targetPage = "vtubelighting.html"; break;
+            case 2: targetPage = "twitch-cat-overlay.html"; break;
+            case 3: targetPage = "go-chicken.html"; break;
+            case 4: targetPage = "strength-quest.html"; break;
+            case 5: targetPage = "operation-earthquake.html"; break;
         }
 
         if (targetPage) window.location = targetPage;
